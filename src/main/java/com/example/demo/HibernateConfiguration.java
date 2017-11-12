b6ba0dd11113d36f0b26fb24930d2921072dafb2
abhilashmedi <abhilashmedi147@gmail.com>
2017-11-12 12:54:42 +0530

    Revert "Update HibernateConfiguration.java"
    This reverts commit fa12887db52e585ec210685760e8af3c5b4f1f65.

--- src/main/java/com/example/demo/HibernateConfiguration.java
+++ src/main/java/com/example/demo/HibernateConfiguration.java
@@ -1,4 +1,72 @@
 package com.example.demo;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
 public class HibernateConfiguration {
+	@Value("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+	private String DRIVER;
+
+	// @Value("abhilash")
+	private String PASSWORD = "AravindReddy0713";
+
+	// @Value("jdbc:sqlserver://ARUN-HP:1433;databaseName=abhilash")
+	private String URL = "jdbc:sqlserver://spring-boot-sqlserver.database.windows.net:1433;database=spring-boot-sqlinstance";
+
+	// @Value("sa")
+	private String USERNAME = "abhilashmedi@spring-boot-sqlserver";
+
+	@Value("org.hibernate.dialect.SQLServerDialect")
+	private String DIALECT;
+
+	@Value("true")
+	private String SHOW_SQL;
+
+	@Value("create")
+	private String HBM2DDL_AUTO;
+
+	@Value("com")
+	private String PACKAGES_TO_SCAN;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(DRIVER);
+		dataSource.setUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
+		return dataSource;
+	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan(PACKAGES_TO_SCAN);
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.put("hibernate.dialect", DIALECT);
+		hibernateProperties.put("hibernate.show_sql", SHOW_SQL);
+		hibernateProperties.put("hibernate.hbm2ddl.auto", HBM2DDL_AUTO);
+		sessionFactory.setHibernateProperties(hibernateProperties);
+
+		return sessionFactory;
+	}
+
+	@Bean
+	public HibernateTransactionManager transactionManager() {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory().getObject());
+		return transactionManager;
+	}
 }
